fef5bb374425ae76d48a9fb0049772c0095dd654
chan9934 <kim75249934@gmail.com>
2025-04-25 07:39:49 +0900

    sln 구성

---
+++ CodingTest/Templates/Template.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <vector>
+#include <string>
+#include <algorithm>
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    // 문제 입력 및 풀이
+    return 0;
+}
