bd7f611fef51c0a75849bd8c0ee41e8822f06026
chan9934 <kim75249934@gmail.com>
2025-04-29 19:50:43 +0900

    [update] 재귀 순열 구현 복습

--- CodingTest/CodingTest/복습.cpp
+++ CodingTest/CodingTest/복습.cpp
@@ -1,19 +1,25 @@
 #include "CodingTest.h"
-int arr[] = { 1, 2, 3, 4, 5 };
+vector<int> vec = { 1,2,3,4,5 };
 int n = 5;
 int r = 3;
 
-void Func_Permutation()
+void Func_Permutation(int n, int r, int depth)
 {
-
+	if (r == depth)
+	{
+		Permutation::Print(vec, r);
+		return;
+	}
+	for (int i = depth; i < n; ++i)
+	{
+		swap(vec[i], vec[depth]);
+		Func_Permutation(n, r, depth + 1);
+		swap(vec[i], vec[depth]);
+	}
 }
 int main()
 {
 
-	do
-	{
-		Permutation::Print(arr, r);
-	} while (next_permutation(&arr[0], &arr[n]));
-	
+	Func_Permutation(5, 3, 0);
 	return 0;
 }
