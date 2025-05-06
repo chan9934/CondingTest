964f9cd301ccaf36a057a51a9652d7992dfc5e8f
chan9934 <kim75249934@gmail.com>
2025-05-06 09:33:54 +0900

    [update] 1620_나는야_포켓몬_마스터_이다솜 입출력 속도 향상의 중요성

--- CodingTest/CodingTest/1620_나는야_포켓몬_마스터_이다솜.cpp
+++ CodingTest/CodingTest/1620_나는야_포켓몬_마스터_이다솜.cpp
@@ -6,36 +6,34 @@
 using namespace std;
 int N, M;
 map<string, int>dogam_strs;
-map<int, string>dogam_numbers;
-vector<string>rets;
+vector<string>dogam;
 
 int main()
 {
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL); cout.tie(NULL);
 	cin >> N >> M;
+	dogam.resize(N + 4);
 	for (int i = 1; i <= N; ++i)
 	{
 		string str;
 		cin >> str;
-		dogam_numbers[i] = str;
+		dogam[i] = str;
 		dogam_strs[str] = i;
 	}
 	for (int i = 0; i < M; ++i)
 	{
 		string matter;
 		cin >> matter;
-		if (isdigit(matter[0]))
+		int number = atoi(matter.c_str());
+		if (number != 0)
 		{
-			rets.emplace_back(dogam_numbers[stoi(matter)]);
+			cout << dogam[number] << "\n";
 		}
 		else
 		{
-			rets.emplace_back(to_string(dogam_strs[matter]));
+			cout << dogam_strs[matter] << "\n";
 		}
-	}
-
-	for (string ret : rets)
-	{
-		cout << ret << endl;
 	}
 	return 0;
 }
