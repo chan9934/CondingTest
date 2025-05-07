bdb88d2bac9dbbe88b64275c36fa94cfe290be6f
chan9934 <kim75249934@gmail.com>
2025-05-07 10:18:41 +0900

    [update] 9375_패션왕_신해빈 문제 풀이

--- CodingTest/CodingTest/9375_패션왕_신해빈.cpp
+++ CodingTest/CodingTest/9375_패션왕_신해빈.cpp
@@ -6,77 +6,7 @@
 using namespace std;
 
 int n, m;
-string input_str;
-map < string, vector<string>>wear_type_map;
-vector<string>wear_type_vec;
-
-void split_str(const string& str, char delimeter, string& left_str, string& right_str)
-{
-	auto find_delimeter = str.find(delimeter);
-	left_str = str.substr(0, find_delimeter);
-	right_str = str.substr(find_delimeter + 1);
-
-}
-
-int combi(int num, int r, vector<int>& ret_indeices, int start_index)
-{
-	static int result_value = 0;
-	if (start_index == -1)
-	{
-		cout << "start_index start combi " << "\n";
-		result_value = 0;
-	}
-	if ((int)ret_indeices.size() == r)
-	{
-		for (int index : ret_indeices)
-		{
-			if ((int)wear_type_vec.size() - 1 >= index)
-			{
-				string wear_type_str = wear_type_vec[index];
-
-				auto wear_names = wear_type_map.find(wear_type_str);
-				if (wear_names == wear_type_map.end())
-				{
-					cout << "calculate error" << "\n";
-					return 0;
-				}
-				return (int)wear_names->second.size();
-			}
-			else
-			{
-				cout << "calculate error" << "\n";
-				return 0;
-			}
-		}
-		return 0;
-	}
-
-	for (int i = start_index + 1; i < num; ++i)
-	{
-		ret_indeices.emplace_back(i);
-		int test = combi(num, r, ret_indeices, i);
-		result_value += test;
-		cout << "test : " << test << " result_value : " << result_value << endl;
-		ret_indeices.pop_back();
-	}
-	return result_value;
-}
-
-int calculate()
-{
-	int result = 0;
-	if (!wear_type_map.empty())
-	{
-		int num = (int)wear_type_map.size();
-		for (int i = 1; i <= num; ++i)
-		{
-			vector<int> indeices;
-			int result_value = combi(num, i, indeices, -1);
-			result += result_value;
-		}
-	}
-	return result;
-}
+string a, b;
 int main()
 {
 	ios_base::sync_with_stdio(false);
@@ -86,24 +16,20 @@ int main()
 	for (int i = 0; i < n; ++i)
 	{
 		cin >> m;
-		wear_type_map.clear();
-		wear_type_vec.clear();
+		map<string, int> type_number;
 		for (int j = 0; j < m; ++j)
 		{
-			string input_str;
-			cin.ignore();
-			getline(cin, input_str);
-
-			string left_str, right_str;
-			split_str(input_str, ' ', left_str, right_str);
-			wear_type_map[right_str].emplace_back(left_str);
-			auto find_vec = find(wear_type_vec.begin(), wear_type_vec.end(), right_str);
-			if (find_vec == wear_type_vec.end())
-			{
-				wear_type_vec.emplace_back(right_str);
-			}
+			cin >> a >> b;
+			type_number[b]++;
 		}
-		cout << calculate() << "\n";
+		long long ret = 1;
+		for (auto number : type_number)
+		{
+			ret *= (long long)(number.second + 1);
+		}
+
+		--ret;
+		cout << ret << "\n";
 
 	}
 	return 0;
