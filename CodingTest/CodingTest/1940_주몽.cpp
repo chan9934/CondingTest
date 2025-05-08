5b3bfff18614900cc3176e80ba29b38ae273ed31
chan9934 <kim75249934@gmail.com>
2025-05-08 09:24:52 +0900

    [update] 1940_주몽 재귀로 문제 풀이

--- CodingTest/CodingTest/1940_주몽.cpp
+++ CodingTest/CodingTest/1940_주몽.cpp
@@ -5,24 +5,6 @@ using namespace std;
 int n, ret_cnt;
 long long m;
 vector<int> v;
-
-
-void func_combi(int startindex, vector<int>& ret_index)
-{
-	if (size(ret_index) == 2)
-	{
-		if ((long long)(v[ret_index[0]] + v[ret_index[1]]) == m)
-			ret_cnt++;
-		return;
-	}
-
-	for (int i = startindex + 1; i < n; ++i)
-	{
-		ret_index.emplace_back(i);
-		func_combi(i, ret_index);
-		ret_index.pop_back();
-	}
-}
 int main()
 {
 	cin >> n >> m;
@@ -34,8 +16,14 @@ int main()
 			continue;
 		v.emplace_back(number);
 	}
-	vector<int> combi_index;
-	func_combi(-1, combi_index);
+	for (int i = 0; i < n; ++i)
+	{
+		for (int j = i + 1; j < n; ++j)
+		{
+			if (v[i] + v[j] == m)
+				++ret_cnt;
+		}
+	}
 	cout << ret_cnt;
 	return 0;
 }
