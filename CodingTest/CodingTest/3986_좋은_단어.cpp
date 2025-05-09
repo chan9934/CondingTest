476fdc1ef1e9f900b4c1e6431f08990334b7b167
chan9934 <kim75249934@gmail.com>
2025-05-09 14:13:58 +0900

    [update] 3986_좋은_단어 인강 시청 후 문제 풀이

--- CodingTest/CodingTest/3986_좋은_단어.cpp
+++ CodingTest/CodingTest/3986_좋은_단어.cpp
@@ -1,59 +1,39 @@
 #include <iostream>
 #include <vector>
 #include <algorithm>
-
+#include <stack>
 using namespace std;
 
-int n, ret_cnt;
+int n, ret;
 
 
 int main()
 {
 	ios_base::sync_with_stdio(false);
 	cin.tie(NULL); cout.tie(NULL);
+
 	cin >> n;
+
 	for (int i = 0; i < n; ++i)
 	{
+
+		stack<char>stack_chr;
 		string str;
 		cin >> str;
-		if (size(str) & 1)continue;
-
+		for (char chr : str)
 		{
-			int A_cnt = 0;
-			int B_cnt = 0;
-			for (char chr : str)
+			if (!stack_chr.empty())
 			{
-				if (chr == 'A')
-					++A_cnt;
-				else if (chr == 'B')
-					++B_cnt;
+				if (chr == stack_chr.top())
+				{
+					stack_chr.pop();
+					continue;
+				}
 			}
-			if (A_cnt & 1 || B_cnt & 1)
-				continue;
-
+			stack_chr.push(chr);
 		}
-		while (true)
-		{
-			if (str.empty())
-				++ret_cnt;
-			else if (str.size() == 1)
-				break;
-
-			auto find_index = str.find("AA");
-			if (string::npos != find_index)
-			{
-				str.erase(find_index, 2);
-				continue;
-			}
-			find_index = str.find("BB");
-			if (string::npos != find_index)
-			{
-				str.erase(find_index, 2);
-				continue;
-			}
-			break;
-		}
+		if (stack_chr.empty()) ++ret;
 	}
-	cout << ret_cnt;
+	cout << ret;
 	return 0;
 }
