27d71f3321b9d7986d6b0283ae6cf91883456d21
chan9934 <kim75249934@gmail.com>
2025-05-09 16:10:40 +0900

    [update] 1629_곱셈 모듈러 연산, 재귀 활용한 풀이

--- CodingTest/CodingTest/1629_곱셈.cpp
+++ CodingTest/CodingTest/1629_곱셈.cpp
@@ -5,67 +5,20 @@
 using namespace std;
 
 int a, b, c;
-long long ret;
-long long ret_2;
-queue<int> get_square_values()
+int  go(int a, int b)
 {
-	queue<int> ret_values;
-	int value = b;
-	int index = 0;
-	while (true)
-	{
-		if (value & 1)
-		{
-			ret_values.push(index);
-			--value;
-		}
+	if (b == 1)
+		return a % c;
 
-		++index;
-		value /= 2;
-		if (value == 1)
-		{
-			ret_values.push(index);
-			return ret_values;
-		}
-	}
+	long long ret = go(a, b / 2);
+	ret = (ret * ret) % c;
+	if (b & 1) ret = (ret * a) % c;
+	return ret;
+
 }
 int main()
 {
 	cin >> a >> b >> c;
-	int square_value = a % c;
-	if (b == 1)
-	{
-		cout << square_value;
-		return 0;
-	}
-	if (b == 0)
-	{
-		cout << 1;
-		return 0;
-	}
-	queue<int> queue_values = get_square_values();
-	if (square_value == 0)
-	{
-		cout << 0;
-		return 0;
-	}
-	int index = 0;
-	ret_2 = 1;
-	while (true)
-	{
-		if (queue_values.empty())
-			break;
-		if (queue_values.front() == index)
-		{
-			ret_2 *= square_value;
-			ret_2 %= c;
-			queue_values.pop();
-		}
-		square_value *= square_value;
-		square_value = square_value % c;
-
-		++index;
-	}
-	cout << ret_2 % c;
+	cout << go(a, b);
 	return 0;
 }
