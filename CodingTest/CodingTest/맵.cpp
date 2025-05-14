5d9185f8ecf51e2b37f954ba2add77e32d153c40
chan9934 <kim75249934@gmail.com>
2025-05-14 09:26:41 +0900

    [update] 맵 테스트 코드 추가 작성

--- CodingTest/CodingTest/맵.cpp
+++ CodingTest/CodingTest/맵.cpp
@@ -2,15 +2,49 @@
 
 using namespace std;
 
-int dy[] = { -1, -1, 0, 1, 1, 1, 0, -1 };
-int dx[] = { 0, 1, 1, 1, 0, -1, -1, -1 };
-int main()
+//int dy[] = { -1, -1, 0, 1, 1, 1, 0, -1 };
+//int dx[] = { 0, 1, 1, 1, 0, -1, -1, -1 };
+//int main()
+//{
+//	int x = 0, y = 0;
+//	for (int i = 0; i < sizeof(dy) / sizeof(int); ++i)
+//	{
+//		int ny = y + dy[i];
+//		int nx = x + dx[i];
+//		cout << ny << " : " << nx << "\n";
+//	}
+//}
+int n;
+int dy[] = { -1, 0, 1, 0 };
+int dx[] = { 0, 1, 0, -1 };
+
+bool visited[3][3];
+int a[3][3];
+
+void go(int y, int x)
 {
-	int x = 0, y = 0;
-	for (int i = 0; i < sizeof(dy) / sizeof(int); ++i)
+	visited[y][x] = true;
+
+	cout << "y : " << y << " x : " << x << "\n";
+
+	for (int i = 0; i < 4; ++i)
 	{
 		int ny = y + dy[i];
 		int nx = x + dx[i];
-		cout << ny << " : " << nx << "\n";
+		if (ny < 0 || ny >= n || nx < 0 || nx >= n)continue;
+		if (a[ny][nx] == 0 || visited[ny][nx])continue;
+		go(ny, nx);
 	}
+}
+int main()
+{
+	cin >> n;
+	for (int i = 0; i < n; ++i)
+	{
+		for (int j = 0; j < n; ++j)
+		{
+			cin >> a[i][j];
+		}
+	}
+	go(0, 0);
 }
