23537c6b78ff47a52bbef2aed6fd06c982a58363
chan9934 <kim75249934@gmail.com>
2025-05-15 10:05:14 +0900

    [update] 1012_유기농_배추 문제 풀이

---
+++ CodingTest/CodingTest/1012_유기농_배추.cpp
@@ -0,0 +1,63 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int t, n, m, k, ret;
+int y, x;
+const int v = 54;
+vector<int>adj[v];
+bool visited[v][v];
+
+int dy[] = { -1, 0, 1, 0 };
+int dx[] = { 0, 1, 0, -1 };
+
+void go(int y, int x)
+{
+	visited[y][x] = true;
+
+	for (int i = 0; i < 4; ++i)
+	{
+		int ny = y + dy[i];
+		int nx = x + dx[i];
+
+		if (ny < 0 || nx < 0 || ny >= 50 || nx >= 50)continue;
+		if (visited[ny][nx] == true)continue;
+		for (int there : adj[ny])
+		{
+			if (there != nx) continue;
+
+			go(ny, nx);
+		}
+	}
+}
+int main()
+{
+	cin >> t;
+
+	for (int i = 0; i < t; ++i)
+	{
+		cin >> m >> n >> k;
+		for (int j = 0; j < k; ++j)
+		{
+			cin >> x >> y;
+			adj[y].push_back(x);
+		}
+		for (int j = 0; j < v; ++j)
+		{
+			for (int there : adj[j])
+			{
+				if (visited[j][there] == true)continue;
+				go(j, there);
+				++ret;
+			}
+		}
+		cout << ret << "\n";
+		memset(visited, 0, sizeof(visited));
+		memset(adj, 0, sizeof(adj));
+
+		ret = 0;
+	}
+	return 0;
+}
