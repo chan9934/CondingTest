cdc199b21c901355d609a0bdae267b1ad6f1cfc3
chan9934 <kim75249934@gmail.com>
2025-05-16 11:38:40 +0900

    [update] 1992_쿼드트리 재귀 풀이

--- CodingTest/CodingTest/1992_쿼드트리.cpp
+++ CodingTest/CodingTest/1992_쿼드트리.cpp
@@ -4,49 +4,31 @@ using namespace std;
 
 int n;
 char a[68][68];
-string ret;
 
-int dy[] = { 0, 0, 1, 1 };
-int dx[] = { 0, 1, 0, 1 };
 
-string go(int s_y, int s_x, int d)
+string quard(int y, int x, int size)
 {
-	string r_s;
-	for (int i = 0; i < 4; ++i)
+	if (size == 1)
+		return string(1, a[y][x]);
+	string ret;
+	char chk = a[y][x];
+	for (int i = y; i < y + size; ++i)
 	{
-		int ny = s_y + dy[i] * d;
-		int nx = s_x + dx[i] * d;
-		if (d == 1)
+		for (int j = x; j < x + size; ++j)
 		{
-			r_s += a[ny][nx];
+			if (chk != a[i][j])
+			{
+				ret = '('
+					+ quard(y, x, size / 2)
+					+ quard(y, x + size / 2, size / 2)
+					+ quard(y + size / 2, x, size / 2)
+					+ quard(y + size / 2, x + size / 2, size / 2)
+					+ ')';
+				return ret;
+			}
 		}
-		else
-		{
-			r_s += go(ny, nx, d / 2);
-		}
 	}
-	if(r_s == "")
-		cout << "ny : " << s_y << " nx : " << s_x << "\n";
-	if (d == 1)
-	{
-		char chk = r_s[0];
-		cout << "chk : " << chk << "\n";
-		for (int i = 1; i < 4; ++i)
-		{
-			if (chk != r_s[i])
-				return '(' + r_s + ')';
-		}
-		r_s = chk;
-		return r_s;
-	}
-	if (r_s.size() != 4)
-	{
-		return '(' + r_s + ')';
-	}
-	else
-	{
-		return r_s;
-	}
+	return string(1, chk);
 }
 
 int main()
@@ -60,7 +42,7 @@ int main()
 			cin >> a[i][j];
 		}
 	}
-	cout << go(0, 0, n);
+	cout << quard(0, 0, n);
 
 	return 0;
 }
