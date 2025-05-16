61178b3bc9958709df101e36c73c446b5779d063
chan9934 <kim75249934@gmail.com>
2025-05-16 09:27:59 +0900

    [update] 2583_영역_구하기 y, x 바꿔서 풀이

--- CodingTest/CodingTest/2583_영역_구하기.cpp
+++ CodingTest/CodingTest/2583_영역_구하기.cpp
@@ -4,27 +4,28 @@
 
 using namespace std;
 
-bool a[104][104];
-bool visited[104][104];
-int m, n, k, ret;
-int s_y, s_x, e_y, e_x;
-vector<int>ret_v;
+#define y1 y1_define
+int a[104][104];
+int visited[104][104];
+int m, n, k;
+int x1, x2, y1, y2;
+vector<int>ret;
 
 int dy[] = { -1, 0, 1, 0 };
 int dx[] = { 0, 1, 0, -1 };
 
 int dfs(int y, int x)
 {
-	visited[y][x] = true;
+	visited[y][x] = 1;
 	int cnt = 1;
 	for (int i = 0; i < 4; ++i)
 	{
 		int ny = y + dy[i];
 		int nx = x + dx[i];
 
-		if (ny < 0 || nx < 0 || ny >= n || nx >= m) continue;
-		if (a[ny][nx] == true)continue;
-		if (visited[ny][nx] == true)continue;
+		if (ny < 0 || nx < 0 || ny >= m || nx >= n) continue;
+		if (a[ny][nx] == 1)continue;
+		if (visited[ny][nx] == 1)continue;
 		cnt += dfs(ny, nx);
 	}
 	return cnt;
@@ -33,32 +34,32 @@ int main()
 {
 	cin >> m >> n >> k;
 
-	for (int t = 0; t < k; ++t)
+	for (int i = 0; i < k; ++i)
 	{
-		cin >> s_y >> s_x >> e_y >> e_x;
-		for (int i = s_y; i < e_y; ++i)
+		cin >> x1 >> y1 >> x2 >> y2;
+		
+		for (int y = y1; y < y2; ++y)
 		{
-			for (int j = s_x; j < e_x; ++j)
+			for (int x = x1; x < x2; ++x)
 			{
-				a[i][j] = true;
+				a[y][x] = 1;
 			}
 		}
 	}
-	for (int i = 0; i < n; ++i)
+	for (int i = 0; i < m; ++i)
 	{
-		for (int j = 0; j < m; ++j)
+		for (int j = 0; j < n; ++j)
 		{
-			if (a[i][j] == false && visited[i][j] == false)
+			if(a[i][j] != 1 && visited[i][j] == 0)
 			{
 				int cnt = dfs(i, j);
-				++ret;
-				ret_v.push_back(cnt);
+				ret.push_back(cnt);
 			}
 		}
 	}
-	cout << ret << "\n";
-	sort(ret_v.begin(), ret_v.end());
-	for (int element : ret_v)
+	cout << ret.size() << "\n";
+	sort(ret.begin(), ret.end());
+	for (int element : ret)
 	{
 		cout << element << " ";
 	}
