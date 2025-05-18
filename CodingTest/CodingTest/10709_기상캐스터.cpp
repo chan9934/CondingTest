6f1bbad59cec8579ace897d9cd983288bfd3de71
chan9934 <kim75249934@gmail.com>
2025-05-18 09:23:43 +0900

    [update] 10709_기상캐스터 문제 풀이

---
+++ CodingTest/CodingTest/10709_기상캐스터.cpp
@@ -0,0 +1,49 @@
+#include <iostream>
+
+using namespace std;
+
+int h, w;
+char c;
+
+int a[104][104];
+
+int main()
+{
+	cin >> h >> w;
+	for (int i = 0; i < h; ++i)
+	{
+		for (int j = 0; j < w; ++j)
+		{
+			cin >> c;
+			if (c == 'c')
+			{
+				a[i][j] = 0;
+			}
+			else
+			{
+				a[i][j] = -1;
+			}
+		}
+	}
+
+	for (int i = 0; i < h; ++i)
+	{
+		for (int j = 0; j < w; ++j)
+		{
+			if (j == 0) continue;
+			if (a[i][j] == 0)continue;
+			int temp = a[i][j - 1];
+			if (temp == -1) continue;
+			a[i][j]= temp + 1;
+		}
+	}
+	for (int i = 0; i < h; ++i)
+	{
+		for (int j = 0; j < w; ++j)
+		{
+			cout << a[i][j] << " ";
+		}
+		cout << "\n";
+	}
+	return 0;
+}
