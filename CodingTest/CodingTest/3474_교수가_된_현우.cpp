9edf406a225cc367fe4842e70ecedea1036f4a95
chan9934 <kim75249934@gmail.com>
2025-05-19 18:25:21 +0900

    [update] 3474_교수가_된_현우 5의 승수 누적합을 사용한 풀이

--- CodingTest/CodingTest/3474_교수가_된_현우.cpp
+++ CodingTest/CodingTest/3474_교수가_된_현우.cpp
@@ -5,78 +5,21 @@
 using namespace std;
 
 int t, n;
-pair<int, int>get(int number)
-{
-	pair<int, int>ret;
-	bool fin_two = false;
-	bool fin_five = false;
-	while (true)
-	{
-		if (!fin_two)
-		{
-			if (number % 2 == 0)
-			{
-				number /= 2;
-				++ret.first;
-			}
-			else
-			{
-				fin_two = true;
-			}
-		}
-		if (!fin_five)
-		{
-			if (number % 5 == 0)
-			{
-				number /= 5;
-				++ret.second;
-			}
-			else
-			{
-				fin_five = true;
-			}
-		}
-		if (fin_two && fin_five)
-			break;
-	}
-	return ret;
-}
-pair<int, int> fac(int number)
-{
-	pair<int, int>ret;
-	for (int i = 2; i <= number;)
-	{
-		cout << i << "\n";
-		pair<int, int>temp = get(i);
-		ret.first += temp.first;
-		ret.second += temp.second;
-		i = i + 2;
-		if(i % 10 == 0)
-			i = i + 2;
-	}
-	for (int i = 5; i <= number;)
-	{
-		cout << i << "\n";
-		pair<int, int>temp = get(i);
-		ret.first += temp.first;
-		ret.second += temp.second;
-		i = i + 5;
-	}
-	return ret;
-}
 int main()
 {
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL); cout.tie(NULL);
 	cin >> t;
 
 	for (int i = 0; i < t; ++i)
 	{
 		cin >> n;
-		pair<int, int>get = fac(n);
-		int left = get.first;
-		int right = get.second;
-
-		int ret = min(left, right);
-		cout << ret << "\n";
+		int cnt = 0;
+		for (int j = 5; j <= n; j *= 5)
+		{
+			cnt += n / j;
+		}
+		cout << cnt << "\n";
 	}
 
 	return 0;
