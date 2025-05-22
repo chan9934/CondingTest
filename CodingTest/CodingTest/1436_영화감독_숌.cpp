fc307b9e0e11a00d5d2e3bb3740197bdef583d06
chan9934 <kim75249934@gmail.com>
2025-05-22 13:40:04 +0900

    [update] 1436_영화감독_숌 못품

--- CodingTest/CodingTest/1436_영화감독_숌.cpp
+++ CodingTest/CodingTest/1436_영화감독_숌.cpp
@@ -9,22 +9,38 @@ string ret;
 
 int main()
 {
-	cin >> n;
+	while(true)
+	{
+		cin >> n;
 
-	s = n / 17;
-	r = n % 17;
-	
-	if (r <= 6)
-	{
-		f = s + r -1;
-		b = 6;
+		s = n / 16;
+		r = n % 16;
+		if (r <= 6)
+		{
+			if (s == 0)
+			{
+				f = r - 1;
+			}
+			else
+			{
+				f = (s - 1) * 10 + 6 + r + 1;
+			}
+			b = 6;
+		}
+		else
+		{
+			if (s == 0)
+			{
+				f =  6;
+			}
+			else
+			{
+				f = (s - 1) * 10 + 6;
+			}
+			b = r - 7;
+		}
+		ret = to_string(f) + "66" + to_string(b);
+		cout << ret << endl;
 	}
-	else
-	{
-		f = s * 10 + 6;
-		b = r - 7;
-	}
-	ret = to_string(f) + "66" + to_string(b);
-	cout << ret << endl;
 	return 0;
 }
