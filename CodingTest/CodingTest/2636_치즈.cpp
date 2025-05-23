c954e43bb16d7ebf76471c4c5b13034bbf1f89f7
chan9934 <kim75249934@gmail.com>
2025-05-23 11:27:09 +0900

    [update] 2636_치즈 다른방식으로 문제 풀이

--- CodingTest/CodingTest/2636_치즈.cpp
+++ CodingTest/CodingTest/2636_치즈.cpp
@@ -1,46 +1,29 @@
 #include <iostream>
+#include <vector>
 
 using namespace std;
 
-int n, m, cnt, ret;
+int n, m, cnt, cnt2;
 int a[104][104];
 int visited[104][104];
-
+vector < pair<int, int>>v;
 int dy[] = { -1, 0, 1, 0 };
 int dx[] = { 0, 1, 0, -1 };
-bool melt_check(int y, int x)
-{
-	bool melt = false;
-	for (int i = 0; i < 4; ++i)
-	{
-		int ny = y + dy[i];
-		int nx = x + dx[i];
-		if (ny < 0 || nx < 0 || ny >= n || nx >= m) continue;
-		if (a[ny][nx] != 1)
-		{
-			melt = true;
-			break;
-		}
-	}
-	return melt;
-}
 void dfs(int y, int x)
 {
 	visited[y][x] = 1;
+	if (a[y][x] == 1)
+	{
+		v.push_back({ y,x });
+		return;
+	}
 	for (int i = 0; i < 4; ++i)
 	{
 		int ny = y + dy[i];
 		int nx = x + dx[i];
 		if (ny < 0 || nx < 0 || ny >= n || nx >= m) continue;
 		if (visited[ny][nx] == 1)continue;
-		if (a[ny][nx] == 0)
-		{
-			dfs(ny, nx);
-		}
-		else
-		{
-			visited[ny][nx] = 1;
-		}
+		dfs(ny, nx);
 	}
 
 }
@@ -52,39 +35,34 @@ int main()
 		for (int j = 0; j < m; ++j)
 		{
 			cin >> a[i][j];
-			if (a[i][j] == 1)
-				++ret;
 		}
 	}
-	int prev = ret;
 	while (true)
 	{
-		if (ret == 0)
-		{
-			cout << cnt << "\n";
-			cout << prev << "\n";
-			break;
-		}
-		prev = ret;
+		fill(&visited[0][0], &visited[0][0] + (104 * 104), 0);
+		v.clear();
 		dfs(0, 0);
 
+		cnt2 = v.size();
+		if (cnt2 == 0)
+			break;
+
+		for (pair<int, int> b : v) {
+			a[b.first][b.second] = 0;
+		}
+		bool flag = true;
 		for (int i = 0; i < n; ++i)
 		{
 			for (int j = 0; j < m; ++j)
 			{
-				if (visited[i][j] == 1)
-				{
-					visited[i][j] = 0;
-					if (a[i][j] == 1)
-					{
-						--ret;
-						a[i][j] = 0;
-					}
-
-				}
+				if (a[i][j] == 1)
+					flag = false;
 			}
 		}
-		
+
 		++cnt;
+		if (flag)
+			break;
 	}
+	cout << cnt << '\n' << cnt2 << '\n';
 }
