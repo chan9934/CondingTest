65668e56cdacd8a6f23d40ebca42b490f3874242
chan9934 <kim75249934@gmail.com>
2025-06-01 09:07:10 +0900

    [update] 12869_뮤탈리스크 재귀로 문제 풀기

--- CodingTest/CodingTest/12869_뮤탈리스크.cpp
+++ CodingTest/CodingTest/12869_뮤탈리스크.cpp
@@ -1,65 +1,53 @@
 #include <iostream>
 #include <vector>
-#include <algorithm>
 
 using namespace std;
 
 int n, ret;
-vector<int>v;
+vector<int> v;
+
+void permutation(vector<int> temp, int depth, int level)
+{
+	if(depth == n)
+	{
+		temp[0] -= 9;
+		temp[1] -= 3;
+		temp[2] -= 1;
+		for (int i = 0; i < n; ++i)
+		{
+			if (temp[i] > 0)
+				break;
+			if (i == n - 1)
+			{
+				ret = min(level, ret);
+				return;
+			}
+		}
+		permutation(temp, 0, level + 1);
+		cout << level + 1 << "\n";
+		
+		return;
+	}
+
+	for (int i = depth; i < n; ++i)
+	{
+		swap(temp[i], temp[depth]);
+		permutation(temp, depth + 1, level);
+		swap(temp[i], temp[depth]);
+	}
+}
 
 int main()
 {
 	cin >> n;
+
 	for (int i = 0; i < n; ++i)
 	{
 		int temp = 0;
 		cin >> temp;
 		v.push_back(temp);
 	}
-	while (true)
-	{
-		++ret;
-		sort(v.begin(), v.end());
-		if (n == 3)
-		{
-
-			if (v[1] == 9)
-			{
-				v[0] -= 1;
-				v[1] -= 9;
-				v[2] -= 3;
-			}
-			else
-			{
-				v[0] -= 1;
-				v[1] -= 3;
-				v[2] -= 9;
-			}
-		}
-		else if (n == 2)
-		{
-			v[0] -= 3;
-			v[1] -= 9;
-		}
-		else
-		{
-			v[0] -= 9;
-		}
-
-		bool finish = true;
-		for (int element : v)
-		{
-			if (element > 0)
-			{
-				finish = false;
-				break;
-			}
-		}
-		if (finish)
-		{
-			cout << ret;
-			break;
-		}
-	}
-	return 0;
+	ret = 100;
+	permutation(v, 0, 0);
+	cout << ret;
 }
