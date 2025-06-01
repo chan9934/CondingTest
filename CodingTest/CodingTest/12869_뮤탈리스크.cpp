2001dd0656f41fcc3f0969769238a4200a599b54
chan9934 <kim75249934@gmail.com>
2025-06-01 09:34:35 +0900

    [add] 16637_괄호_추가하기 추가

--- CodingTest/CodingTest/12869_뮤탈리스크.cpp
+++ CodingTest/CodingTest/12869_뮤탈리스크.cpp
@@ -1,56 +1,31 @@
-#include <iostream>
-#include <queue>
-#include <tuple>
-
+#include <bits/stdc++.h>
 using namespace std;
 
-int n, ret;
-int a[3];
-int visited[10][10][10];
-int d[6][3] = {
-	{9,3,1},
-	{9,1,3},
-	{3,9,1},
-	{3,1,9},
-	{1,3,9},
-	{1,9,3},
-};
-struct A {
-	int a, b, c;
-};
-int bfs(int a, int b, int c)
-{
-	visited[a][b][c] = 1;
-	queue<A> q;
-	q.push({ a,b,c });
+int n, ret = 60, a[5], t[3] = { 9, 3, 1 };
+vector<int> v;
 
-	while (q.size())
-	{
-		a = q.front().a;
-		b = q.front().b;
-		c = q.front().c;
-		q.pop();
-		if (visited[0][0][0]) break;
-		for (int i = 0; i < 6; ++i)
-		{
-			int na = max(0, a - d[i][0]);
-			int nb = max(0, b - d[i][1]);
-			int nc = max(0, c - d[i][2]);
-			if (visited[na][nb][nc])continue;
-			visited[na][nb][nc] = visited[a][b][c] + 1;
-			q.push({ na, nb, nc });
-		}
-	}
-	return visited[0][0][0] - 1;
+void go(vector<int> v, int s) {
+    if (*max_element(a, a + 3) <= 0) {
+        ret = min(ret, s);
+        return;
+    }
+    do {
+        if (s + 1 < ret) {
+            for (int i = 0; i < n; i++) a[v[i]] -= t[i];
+            go(v, s + 1);
+            for (int i = 0; i < n; i++) a[v[i]] += t[i];
+        }
+    } while (next_permutation(v.begin(), v.end()));
 }
 
-int main()
-{
-	ios_base::sync_with_stdio(false);
-	cin.tie(NULL);
-	cout.tie(NULL);
-	cin >> n;
-	for (int i = 0; i < n; i++) cin >> a[i];
-	cout << bfs(a[0], a[1], a[2]) << "\n";
-	return 0;
+int main() {
+    ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+    cin >> n;
+    for (int i = 0; i < n; i++) {
+        cin >> a[i];
+    }
+    for (int i = 0; i < n; i++) v.push_back(i);
+    go(v, 0);
+    cout << ret << "\n";
+    return 0;
 }
