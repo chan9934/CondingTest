b6b0d20691364e032c8644ce269a65ddd3e07151
chan9934 <kim75249934@gmail.com>
2025-06-04 11:02:49 +0900

    [update] 13913_숨바꼭질_4 메모리, 시간 초과

--- CodingTest/CodingTest/13913_숨바꼭질_4.cpp
+++ CodingTest/CodingTest/13913_숨바꼭질_4.cpp
@@ -1,27 +1,36 @@
 #include <iostream>
 #include <queue>
-#include <vector>
+#include <map>
 
 using namespace std;
 
 int n, k, ret;
 
 int visited[100004];
-vector<vector<int>> v;
+map<int, int> m;
 
 void bfs(int x)
 {
 	queue<int> q;
 	q.push(x);
+	visited[x] = 1;
 
 	while (q.size())
 	{
 		x = q.front();
 		q.pop();
-
 		for (int i : { x - 1, x + 1, x * 2})
 		{
 			if (i < 0 || i > 100000)continue;
+
+			if (visited[i] == 0)
+			{
+				visited[i] = visited[x] + 1;
+				m[i] = x;
+				if (i == k)
+					return;
+				q.push(i);
+			}
 		}
 	}
 
@@ -30,7 +39,33 @@ void bfs(int x)
 int main()
 {
 	cin >> n >> k;
+	if (n == k)
+	{
+		cout << 0 << "\n" << n;
+		return 0;
+	}
 
 	bfs(n);
+
+	cout << visited[k] - 1 << '\n';
+
+	vector<int> ret;
+	ret.push_back(k);
+	int index = m[k];
+	ret.push_back(index);
+	while (true)
+	{
+		if (m.end() != m.find(index))
+		{
+			index = m[index];
+			ret.push_back(index);
+		}
+		else break;
+	}
+	reverse(ret.begin(), ret.end());
+	for (int element : ret)
+	{
+		cout << element << " ";
+	}
 	return 0;
 }
