649224fd562317f6caa8647ee46f053c08ffb963
chan9934 <kim75249934@gmail.com>
2025-06-06 09:34:36 +0900

    [update] 17071_숨바꼭질_5 홀수 짝수 이해가 안됨

--- CodingTest/CodingTest/17071_숨바꼭질_5.cpp
+++ CodingTest/CodingTest/17071_숨바꼭질_5.cpp
@@ -2,68 +2,41 @@
 #include <queue>
 
 using namespace std;
-
-int n, k, ret;
-
-const int max_n = 500004;
-int visited[max_n];
-
-pair<int, int> dn[] = { {-1, 1}, {1, 1},{0, 2} };
-
-void bfs(int here)
-{
-	queue<int>q;
-	q.push(here);
-	visited[here] = 1;
-
-	int current_level = 0;
-	while (q.size())
-	{
-		int qsize = q.size();
-		++current_level;
-		k += current_level;
-		for (int i = 0; i < qsize; ++i)
-		{
-			here = q.front();
-			q.pop();
-
-			for (int i = 0; i < 3; ++i)
-			{
-				int next = (here * dn[i].second) + dn[i].first;
-
-				if (current_level <= 6)
-				{
-					cout << "next : " << next << " here : " << here << " Current_level : " << current_level << " k : " << k << "\n";
-				}
-				if (next < 0 || next > 500000)continue;
-
-				if (next == k)
-				{
-					ret = current_level;
-					return;
-				}
-				visited[next] = visited[here] + 1;
-				q.push(next);
-			}
-		}
-	}
-}
-int main()
-{
-	cin >> n >> k;
-
-	ios_base::sync_with_stdio(false);
-	cout.tie(NULL); cin.tie(NULL);
-
-	if (n == k)
-	{
-		cout << 0;
-		return 0;
-	}
-	ret = -1;
-	bfs(n);
-
-	cout << ret;
-
-	return 0;
+const int max_n = 500000;
+int visited[2][max_n + 4], a, b, ok, turn = 1;
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    cin >> a >> b;
+    if (a == b) { cout << 0 << "\n"; return 0; }
+    queue<int> q;
+    visited[0][a] = 1;
+    q.push(a);
+    while (q.size()) {
+        b += turn;
+        if (b > max_n)break;
+        if (visited[turn % 2][b]) {
+            ok = true;
+            break;
+        }
+        int qSize = q.size();
+        for (int i = 0; i < qSize; i++) {
+            int x = q.front(); q.pop();
+            for (int nx : {x + 1, x - 1, x * 2}) {
+                if (nx < 0 || nx > max_n || visited[turn % 2][nx]) continue;
+                visited[turn % 2][nx] = visited[(turn + 1) % 2][x] + 1;
+                if (nx == b) {
+                    ok = 1; break;
+                }
+                q.push(nx);
+            }
+            if (ok)break;
+        }
+        if (ok)break;
+        turn++;
+    }
+    if (ok) cout << turn << "\n";
+    else cout << -1 << "\n";
+    return 0;
 }
