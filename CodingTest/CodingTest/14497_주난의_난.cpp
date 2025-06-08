4cf71c232c0f0b0f6821ffe1e148790c0b0d3670
chan9934 <kim75249934@gmail.com>
2025-06-09 08:43:58 +0900

    [update] 14497_주난의_난 문제 풀이

--- CodingTest/CodingTest/14497_주난의_난.cpp
+++ CodingTest/CodingTest/14497_주난의_난.cpp
@@ -2,86 +2,64 @@
 #include <queue>
 
 using namespace std;
+#define y1 y1_real
 
-#define y1 actual_y1
 int n, m;
+const int max_n = 300;
+char a[max_n + 4][max_n + 4];
+int visited[max_n + 4][max_n + 4];
 int x1, y1, x2, y2;
-char a[304][304];
-int visited[304][304];
-
 int dy[] = { -1, 0, 1, 0 };
 int dx[] = { 0, 1, 0, -1 };
 
-void bfs()
+void bfs(int y, int x)
 {
-	visited[x1 - 1][y1 - 1] = 1;
-	queue<pair<int, int>>q_0;
-	queue<pair<int, int>>q_1;
-	q_0.push({ x1 - 1, y1 - 1 });
-	while (q_0.size() || q_1.size())
+	queue<int>q;
+	queue<int>temp;
+	q.push(1000 * y + x);
+	visited[y][x] = 1;
+	while (q.size())
 	{
-		while (q_0.size())
+		while (q.size())
 		{
-			pair<int, int> here = q_0.front();
-			q_0.pop();
+			y = q.front() / 1000;
+			x = q.front() % 1000;
+			q.pop();
 
 			for (int i = 0; i < 4; ++i)
 			{
-				int ny = here.first + dy[i];
-				int nx = here.second + dx[i];
+				int ny = y + dy[i];
+				int nx = x + dx[i];
 
-				if (ny < 0 || nx < 0 || ny> 300 || nx>300)continue;
-				if (visited[ny][nx]) continue;
-
-				if (a[ny][nx] == '1')
+				if (ny < 0 || nx < 0 || ny > n || nx > m)continue;
+				if (visited[ny][nx])continue;
+				if (a[ny][nx] == '0')
 				{
-					q_1.push({ ny, nx });
-					visited[ny][nx] = visited[here.first][here.second] + 1;
+					visited[ny][nx] = visited[y][x];
+					q.push(1000 * ny + nx);
 				}
-				else if (a[ny][nx] == '0')
+				else if (a[ny][nx] == '1')
 				{
-					q_0.push({ ny, nx });
-					visited[ny][nx] = visited[here.first][here.second];
+					visited[ny][nx] = visited[y][x] + 1;
+					temp.push(1000 * ny + nx);
 				}
-			}
-		}
-		while (q_1.size())
-		{
-			pair<int, int> here = q_1.front();
-			q_1.pop();
-
-			for (int i = 0; i < 4; ++i)
-			{
-				int ny = here.first + dy[i];
-				int nx = here.second + dx[i];
-
-				if (ny < 0 || nx < 0 || ny> 300 || nx>300)continue;
-
-				if (visited[ny][nx]) continue;
-
-				if (a[ny][nx] == '1')
+				else if (a[ny][nx] == '#')
 				{
-					q_1.push({ ny, nx });
-					visited[ny][nx] = visited[here.first][here.second] + 1;
-				}
-				else if (a[ny][nx] == '0')
-				{
-					q_0.push({ ny, nx });
-					visited[ny][nx] = visited[here.first][here.second];
+					visited[ny][nx] = visited[y][x];
+					return;
 				}
 			}
 		}
+		q = temp;
 	}
 }
 int main()
 {
+	ios_base::sync_with_stdio(false);
+	cout.tie(NULL); cin.tie(NULL);
 	cin >> n >> m;
 	cin >> x1 >> y1 >> x2 >> y2;
-	if (x1 == x2 && y1 == y2)
-	{
-		cout << 0;
-		return 0;
-	}
+	--x1, --y1, --x2, --y2;
 
 	for (int i = 0; i < n; ++i)
 	{
@@ -90,7 +68,8 @@ int main()
 			cin >> a[i][j];
 		}
 	}
-	bfs();
-	cout << visited[x2 - 1][y2 - 1];
-	return 0;
+
+	bfs(x1, y1);
+
+	cout << visited[x2][y2];
 }
