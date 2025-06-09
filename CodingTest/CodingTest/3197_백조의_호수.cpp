0223a316cef4c50dd4c9f16b19a97dbd68e0d285
chan9934 <kim75249934@gmail.com>
2025-06-09 10:28:10 +0900

    [update] 3197_백조의_호수 q 두개씩 사용

--- CodingTest/CodingTest/3197_백조의_호수.cpp
+++ CodingTest/CodingTest/3197_백조의_호수.cpp
@@ -7,93 +7,56 @@ using namespace std;
 int r, c;
 const int max_n = 1500;
 char a[max_n + 4][max_n + 4];
-int visited[max_n + 4][max_n + 4];
-int l_visited[max_n + 4][max_n + 4];
+int visited_water[max_n + 4][max_n + 4], visited_swan[max_n + 4][max_n + 4];
+queue<pair<int, int>>water_q, water_temp_q, swan_q, swan_temp_q;
+
 int dy[] = { -1, 0, 1, 0 };
 int dx[] = { 0, 1, 0, -1 };
-vector<pair<int, int>> l_position;
-bool bfs(int y, int x, int level)
-{
-	visited[y][x] = level;
 
-	for (int i = 0; i < 4; ++i)
+void q_clear(queue<pair<int, int>>& q)
+{
+	queue<pair<int, int>>().swap(q);
+}
+
+bool move_swan()
+{
+	while (swan_q.size())
 	{
-		if (a[y][x] == '.')
+		int y = swan_q.front().first;
+		int x = swan_q.front().second;
+		swan_q.pop();
+		for (int i = 0; i < 4; ++i)
 		{
-			return true;
-		}
-		int ny = y + dy[i];
-		int nx = x + dx[i];
-		if (ny < 0 || nx < 0 || ny>= r || nx >= c)continue;
-		if (visited[ny][nx] == level)continue;
-		if (a[ny][nx] == '.')
-		{
-			a[y][x] = '.';
+			int ny = y + dy[i];
+			int nx = x + dx[i];
+
+			if (ny < 0 || nx < 0 || ny >= r || ny >= x)continue;
+			if (visited_swan[ny][nx])continue;
+			visited_swan[y][x] = 1;
+			if (a[ny][nx] == 'L') return true;
+			if (a[ny][nx] == '.')swan_q.push({ ny, nx });
+			else if(a[ny][nx] == 'X')swan_temp_q.push({ ny, nx });
 		}
 	}
 	return false;
 }
-void connect(bool& success, int y, int x)
+void move_water()
 {
-	cout << "y : " << y << " x : " << x << "\n";
-	if (y == l_position[1].first && x == l_position[1].second)
+	while (water_q.size())
 	{
-		success = true;
-	}
-	if (success)
-		return;
-	l_visited[y][x] = 1;
-	for (int i = 0; i < 4; ++i)
-	{
-		int ny = y + dy[i];
-		int nx = x + dx[i];
-
-		if (ny < 0 || nx < 0 || ny>= r || nx >= c)continue;
-		if (l_visited[ny][nx])continue;
-		if (a[ny][nx] != 'X')
+		int y = water_q.front().first;
+		int x = water_q.front().second;
+		water_q.pop();
+		for (int i = 0; i < 4; ++i)
 		{
-			connect(success, ny, nx);
+			int ny = y + dy[i];
+			int nx = x + dx[i];
+
+			if (ny < 0 || nx < 0 || ny >= r || ny >= x)continue;
+			if (visited_water[ny][nx])continue;
+			visited_water[y][x] = 1;
+			if (a[ny][nx] == '.')water_q.push({ ny, nx });
+			else water_temp_q.push({ ny, nx });
 		}
 	}
-
 }
-
-int main()
-{
-	cin >> r >> c;
-
-	for (int i = 0; i < r; ++i)
-	{
-		for (int j = 0; j < c; ++j)
-		{
-			cin >> a[i][j];
-			if (a[i][j] == 'L')
-				l_position.push_back({ i, j });
-		}
-	}
-
-	int level = 1;
-	while (true)
-	{
-		for (int i = 0; i < r; ++i)
-		{
-			for (int j = 0; j < c; ++j)
-			{
-				if (a[i][j] == 'X' && visited[i][j] != level)
-				{
-					bfs(i, j, level);
-				}
-			}
-		}
-		bool success = false;
-		connect(success, l_position[0].first, l_position[0].second);
-
-		if (success)
-			break;
-		fill(&l_visited[0][0], &l_visited[0][0] + (max_n + 4) * (max_n + 4), 0);
-		++level;
-	}
-	cout << level;
-	return 0;
-}
-
