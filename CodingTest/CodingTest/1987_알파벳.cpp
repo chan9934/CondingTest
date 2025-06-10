d4b3056a93dc0f3d0d3dbdde1b766d838ae197e4
chan9934 <kim75249934@gmail.com>
2025-06-11 07:58:43 +0900

    [update] 2529_부등호 재풀이

--- CodingTest/CodingTest/1987_알파벳.cpp
+++ CodingTest/CodingTest/1987_알파벳.cpp
@@ -2,35 +2,36 @@
 
 using namespace std;
 
-int r, c, ret = 1;
-
 const int max_n = 21;
 char a[max_n][max_n];
-char c_visited[26];
-
+bool visited[26];
+int r, c, ret;
 int dy[] = { -1, 0, 1, 0 };
 int dx[] = { 0, 1, 0, -1 };
-void dfs(int y, int x, int depth)
+
+void dfs(int y, int x, int& cnt)
 {
-	ret = max(ret, depth);
+	ret = max(ret, cnt);
 	for (int i = 0; i < 4; ++i)
 	{
 		int ny = y + dy[i];
 		int nx = x + dx[i];
 
 		if (ny < 0 || nx < 0 || ny >= r || nx >= c)continue;
-		if (c_visited[a[ny][nx] - 'A'] == 0)
-		{
-			c_visited[a[ny][nx] - 'A'] = 1;
-			dfs(ny, nx, depth + 1);
-			c_visited[a[ny][nx] - 'A'] = 0;
-		}
+		if (visited[a[ny][nx] - 'A'] == 1)continue;
+
+
+		visited[a[ny][nx] - 'A'] = 1;
+		++cnt;
+		dfs(ny, nx, cnt);
+		visited[a[ny][nx] - 'A'] = 0;
+		--cnt;
 	}
 }
 int main()
 {
 	cin >> r >> c;
-
+	ret = 0;
 	for (int i = 0; i < r; ++i)
 	{
 		for (int j = 0; j < c; ++j)
@@ -38,8 +39,8 @@ int main()
 			cin >> a[i][j];
 		}
 	}
-
-	c_visited[a[0][0] - 'A'] = 1;
-	dfs(0, 0, 1);
+	visited[a[0][0] - 'A'] = 1;
+	int cnt = 1;
+	dfs(0, 0, cnt);
 	cout << ret;
 }
