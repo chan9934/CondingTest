3cc9432d64eb878d21d896eee18f6d853fca2c69
chan9934 <kim75249934@gmail.com>
2025-06-14 09:14:02 +0900

    [update] 15684_사다리_조작 내일 다시 풀기

--- CodingTest/CodingTest/15684_사다리_조작.cpp
+++ CodingTest/CodingTest/15684_사다리_조작.cpp
@@ -1,80 +1,41 @@
-#include <iostream>
-
+#include <bits/stdc++.h>
 using namespace std;
-
-int n, m, h;
-int a[14][34];
-
-bool check(int num, int depth)
-{
-	int check_num = num;
-	while (true)
-	{
-		if (depth == h)
-		{
-			if (a[num][depth])
-			{
-				if (num + 1 == check_num)
-					return true;
-			}
-			else if (num - 1 >= 1 && a[n - 1][depth])
-			{
-				if (num - 1 == check_num)
-					return true;
-			}
-			return false;
-		}
-		if (a[num][depth])
-		{
-			num = num + 1;
-			depth = depth + 1;
-		}
-		else if (n - 1 >= 1 && a[n - 1][depth])
-		{
-			num = num - 1;
-			depth = depth + 1;
-		}
-		else
-		{
-			depth = depth + 1;
-		}
-	}
+const int INF = 987654321;
+int n, m, h, a, b, ret = INF, visited[34][34];
+bool check() {
+    for (int i = 1; i <= n; i++) {
+        int start = i;
+        for (int j = 1; j <= h; j++) {
+            if (visited[j][start])start++;
+            else if (visited[j][start - 1])start--;
+        }
+        if (start != i) return false;
+    }
+    return true;
 }
-
-int main()
-{
-	cin >> n >> m >> h;
-	for (int i = 0; i < m; ++i)
-	{
-		int left = 0;
-		int depth = 0;
-		cin >> left >> depth;
-		a[left][depth] = 1;
-	}
-	int current_check_num = 1;
-	while(current_check_num <= 3)
-	{
-		for (int i = 1; i <= n * h; ++i)
-		{
-			if (current_check_num >= 2)
-			{
-				for (int j = i + 1; j <= n * h; ++j)
-				{
-					if (current_check_num >= 3)
-					{
-						for (int k = j + 1; k <= n * h; ++k)
-						{
-
-						}
-					}
-				}
-			}
-			else
-			{
-				int
-			}
-		}
-		++current_check_num;
-	}
-	return 0;
+void go(int here, int cnt) {
+    if (cnt > 3 || cnt >= ret) return;
+    if (check()) {
+        ret = min(ret, cnt); return;
+    }
+    for (int i = here; i <= h; i++) {
+        for (int j = 1; j < n; j++) {
+            if (visited[i][j] || visited[i][j - 1] || visited[i][j + 1]) continue;
+            visited[i][j] = 1;
+            go(i, cnt + 1);
+            visited[i][j] = 0;
+        }
+    }
+}
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    cin >> n >> m >> h;
+    for (int i = 0; i < m; i++) {
+        cin >> a >> b; visited[a][b] = 1;
+    }
+    go(1, 0);
+    cout << ((ret == INF) ? -1 : ret) << "\n";
+    return 0;
 }
