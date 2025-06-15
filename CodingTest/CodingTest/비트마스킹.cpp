a9c79767f735491014570d579f8eaa24fddb0ab4
chan9934 <kim75249934@gmail.com>
2025-06-16 08:36:09 +0900

    [.update] 비트마스킹 복습

--- CodingTest/CodingTest/비트마스킹.cpp
+++ CodingTest/CodingTest/비트마스킹.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <bitset>
 
 using namespace std;
 
@@ -7,5 +8,12 @@ int main()
 	int a = 5; // 0101
 	int b = 10; // 1010
 
-	int c = a + b; // 1111
+	cout << bitset<8>(a + b) << "\n"; // 1111 
+	cout << bitset<8>(a & b) << "\n"; // 0000
+	cout << bitset<8>(a | b) << "\n"; // 1111
+	cout << bitset<8>(1 << a) << "\n"; // 2^a
+	cout << bitset<8>(2 >> 1) << "\n"; // (1/2)^b
+	cout << bitset<8>(a ^ b) << "\n"; // 1111
+	cout << bitset<8>(~a) << "\n"; // 11111010 ~a = -(a + 1) -> -a = ~a + 1
+	cout << bitset<8>(a &= ~(1 << 2)); // 0001 
 }
