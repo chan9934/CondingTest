2b7834d0baa9f89a0db34e90f4ea9425059777fc
chan9934 <kim75249934@gmail.com>
2025-06-16 11:39:41 +0900

    [update] 비스마스킹을 이용한 경우의 수

--- CodingTest/CodingTest/비트마스킹.cpp
+++ CodingTest/CodingTest/비트마스킹.cpp
@@ -3,17 +3,20 @@
 
 using namespace std;
 
+const int n = 4;
 int main()
 {
-	int a = 5; // 0101
-	int b = 10; // 1010
-
-	cout << bitset<8>(a + b) << "\n"; // 1111 
-	cout << bitset<8>(a & b) << "\n"; // 0000
-	cout << bitset<8>(a | b) << "\n"; // 1111
-	cout << bitset<8>(1 << a) << "\n"; // 2^a
-	cout << bitset<8>(2 >> 1) << "\n"; // (1/2)^b
-	cout << bitset<8>(a ^ b) << "\n"; // 1111
-	cout << bitset<8>(~a) << "\n"; // 11111010 ~a = -(a + 1) -> -a = ~a + 1
-	cout << bitset<8>(a &= ~(1 << 2)); // 0001 
+	string a[n] = { "사과", "딸기", "포도", "배" };
+	for (int i = 0; i < (1 << n); ++i)
+	{
+		string ret = "";
+		for (int j = 0; j < n; ++j)
+		{
+			if (i & (1 << j))
+			{
+				ret += (a[j] + " ");
+			}
+		}
+		cout << ret << "\n";
+	}
 }
