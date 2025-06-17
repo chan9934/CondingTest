fb53510ce42a46d44424afb28823ca4dbe18721b
chan9934 <kim75249934@gmail.com>
2025-06-18 08:57:31 +0900

    [update] 1285_동전_뒤집기 문제 풀이

--- CodingTest/CodingTest/1285_동전_뒤집기.cpp
+++ CodingTest/CodingTest/1285_동전_뒤집기.cpp
@@ -1,27 +1,26 @@
-#include <iostream>
+#include<iostream>
+#include<bitset>
 
 using namespace std;
+
 int n, ret;
 int a[24];
 string s;
-
 void go(int here)
 {
-	if (here + 1 > n)
+	if (here == n)
 	{
 		int sum = 0;
-		for (int i = 1; i <= (1<<n); i *= 2)
+		for (int i = 1; i < (1<<n); i *= 2)
 		{
 			int cnt = 0;
 			for (int j = 0; j < n; ++j)
 			{
-				if (a[i] & j)
-				{
-					++cnt;
-				}
+				if (a[j] & i) ++cnt;
 			}
 			sum += min(cnt, n - cnt);
 		}
+		int temp = ret;
 		ret = min(ret, sum);
 		return;
 	}
@@ -32,19 +31,18 @@ void go(int here)
 int main()
 {
 	cin >> n;
+	ret = 400;
 	for (int i = 0; i < n; ++i)
 	{
 		cin >> s;
 		int value = 1;
-		for (int j = 0; j < s.size(); ++j)
+		for (int j = 0; j < n; ++j)
 		{
-			if (s[j] == 'T')
-				a[i] += value;
+			if (s[j] == 'T') a[i] |= value;
 			value *= 2;
 		}
 	}
-
 	go(0);
-	cout << ret << "\n";
+	cout << ret;
 	return 0;
 }
