88a0399f8c8ddec0b8cf749c0d641baa41ceef5b
chan9934 <kim75249934@gmail.com>
2025-06-18 10:55:10 +0900

    [update] 1987_알파벳_비트마스킹 풀이

---
+++ CodingTest/CodingTest/1987_알파벳_비트마스킹.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+
+using namespace std;
+
+int check;
+char a[24][24];
+int r, c;
+int dy[] = { -1, 0, 1, 0 };
+int dx[] = { 0, 1, 0, -1 };
+
+int go(int y, int x)
+{
+	int ret = 1;
+	for (int i = 0; i < 4; ++i)
+	{
+		int ny = y + dy[i];
+		int nx = x + dx[i];
+
+		if (ny < 0 || nx < 0 || ny >= r || nx >= c)continue;
+		if (check & (1 << int(a[ny][nx])))continue;
+
+		check |= (1 << int(a[ny][nx]));
+		ret += go(ny, nx);
+	}
+	return ret;
+}
+int main()
+{
+	cin >> r >> c;
+	for (int i = 0; i < r; ++i)
+	{
+		for (int j = 0; j < c; ++j)
+		{
+			cin >> a[i][j];
+		}
+	}
+	check |= (1 << int(a[0][0]));
+	cout << go(0, 0) << "\n";
+	return 0;
+}
