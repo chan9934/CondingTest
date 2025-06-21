10d3a86d6fc26c22137d2a1c72afe6bab7a3c851
chan9934 <kim75249934@gmail.com>
2025-06-21 11:11:43 +0900

    [update] 17471_게리맨더링 재풀이

--- CodingTest/CodingTest/17471_게리맨더링.cpp
+++ CodingTest/CodingTest/17471_게리맨더링.cpp
@@ -3,62 +3,76 @@
 
 using namespace std;
 
-int n, m, ret;
-int a[14];
-vector<int>adj[14];
-int visited[14];
-int component[14];
-pair<int, int>dfs(int here, int value)
-{
-	visited[here] = 1;
-	pair<int, int> p = { 1, a[here]};
-	for (auto next : adj[here])
-	{
-		if (visited[next])continue;
-		if (component[next] != value)continue;
+const int max_ret = 1000;
+int n, m, ret = max_ret;
+vector<int>v[11];
+int a[11];
+int comp[11];
+int visited;
 
-		pair<int, int> _temp = dfs(next, value);
-		p.first += _temp.first;
-		p.second += _temp.second;
+pair<int, int> dfs(int here, int index)
+{
+	visited |= (1 << here);
+	pair<int, int> ret = { 1, a[here] };
+	for (int next : v[here])
+	{
+		if (comp[next] != index)continue;
+		if (visited & (1 << next)) continue;
+		pair<int, int> temp = dfs(next, index);
+		ret.first += temp.first;
+		ret.second += temp.second;
 	}
-	return p;
+	return ret;
 }
 
 int main()
 {
 	cin >> n;
-	for (int i = 0; i < n; ++i)
+
+	for (int i = 1; i <= n; ++i)
 	{
-		cin >> a[i];
+		int temp = 0;
+		cin >> temp;
+		a[i] = temp;
 	}
-	for (int i = 0; i < n; ++i)
+
+	for (int i = 1; i <= n; ++i)
 	{
 		cin >> m;
+		int temp = 0;
 		for (int j = 0; j < m; ++j)
 		{
-			int temp = 0;
 			cin >> temp;
-			adj[i].push_back(temp -1);
+			v[i].push_back(temp);
 		}
 	}
-	ret = 1000;
-	for (int i = 1; i < (1 << n) - 1; ++i)
-	{
-		fill(&component[0], &component[0] + 14, 0);
-		fill(&visited[0], &visited[0] + 14, 0);
 
-		int idx1 = -1;
-		int idx2 = -1;
-		for (int j = 0; j < n; ++j)
+	for (int i = 1; i < (1 << n); ++i)
+	{
+		visited = 0;
+		fill(&comp[0], &comp[0] + 11, 0);
+		int comp_0 = 0;
+		int comp_1 = 0;
+		for (int j = 1; j <= n; ++j)
 		{
-			if (i & (1 << j))component[j] = 1, idx1 = j;
-			else idx2 = j;
+			if (i & (1 << (j-1)))
+			{
+				comp[j] = 1; comp_1 = j;
+			}
+			else
+			{
+				comp_0 = j;
+			}
 		}
-		if (idx1 == -1 || idx2 == -1)continue;
-		pair<int, int>comp1 = dfs(idx1, 1);
-		pair<int, int>comp2 = dfs(idx2, 0);
-		if (comp1.first + comp2.first == n) ret = min(ret, abs(comp1.second - comp2.second));
+		if (comp_1 == 0 || comp_0 == 0)continue;
+
+		pair<int, int>first = dfs(comp_1, 1);
+		pair<int, int>second = dfs(comp_0, 0);
+		if (first.first + second.first != n)continue;
+
+		ret = min(ret, abs(first.second - second.second));
 	}
-	cout << (ret == 1000 ? -1:ret) << "\n";
+	if (ret == max_ret)cout << -1 << "\n";
+	else cout << ret << "\n";
 	return 0;
 }
