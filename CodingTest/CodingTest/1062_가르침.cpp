8cf6a577db973e9a320ed530a2b32048ea0d9a7c
chan9934 <kim75249934@gmail.com>
2025-06-22 10:58:29 +0900

    [update] 1062_가르침 풀이

---
+++ CodingTest/CodingTest/1062_가르침.cpp
@@ -0,0 +1,89 @@
+#include<iostream>
+#include<vector>
+#include<bitset>
+#include<string>
+
+using namespace std;
+
+int n, k, ret, check;
+vector<string >s;
+
+int calcul()
+{
+	int num;
+	for (const string& element_1 : s)
+	{
+		bool success = true;
+		for (char element_2 : element_1)
+		{
+			if (check & (1 << (element_2 - 'a')))continue;
+			success = false;
+			break;
+		}
+		if (success) ++num;
+	}
+	return num;
+}
+void extract(string& s)
+{
+	s.erase(s.begin(), s.begin() + 4);
+	s.erase(s.end()-4, s.end());
+
+}
+void combi(vector<int>&indecies, int s_i)
+{
+	if (indecies.size() == k)
+	{
+		int temp = check;
+		int i = 0;
+		for (i; i < indecies.size(); ++i)
+		{
+			int bit = (1 << i);
+			if (check & bit)
+			{
+				check = temp;
+				return;
+			}
+			check |= bit;
+		}
+		ret = max(ret, calcul());
+		check = temp;
+		return;
+	}
+
+	for (int i = s_i + 1; i < 26; ++i)
+	{
+		indecies.push_back(i);
+		combi(indecies, i);
+		indecies.pop_back();
+	}
+}
+int main()
+{
+	cin >> n >> k;
+	ret = 0;
+	if (k < 5)
+	{
+		cout << 0 << "\n";
+		return 0;
+	}
+	k -= 5;
+	check |= (1 << 'a' - 'a');
+	check |= (1 << 'n' - 'a');
+	check |= (1 << 't' - 'a');
+	check |= (1 << 'i' - 'a');
+	check |= (1 << 'c' - 'a');
+	cout << bitset<26>(check) << "\n";
+	for (int i = 0; i < n; ++i)
+	{
+		string temp;
+		cin >> temp;
+		extract(temp);
+		s.push_back(temp);
+	}
+	vector<int>indecies;
+	combi(indecies, -1);
+
+	cout << ret << "\n";
+	return 0;
+}
