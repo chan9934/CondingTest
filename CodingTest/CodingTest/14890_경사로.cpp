9615d634882510737ac01f2135f32050fe62fe2d
chan9934 <kim75249934@gmail.com>
2025-06-23 08:44:38 +0900

    [update] 14890_경사로 재풀이

--- CodingTest/CodingTest/14890_경사로.cpp
+++ CodingTest/CodingTest/14890_경사로.cpp
@@ -6,7 +6,7 @@ int n, l, ret;
 int a[104][104];
 int b[104][104];
 
-void check(int a[104][104])
+void go(int a[104][104])
 {
 	for (int i = 0; i < n; ++i)
 	{
@@ -14,26 +14,45 @@ void check(int a[104][104])
 		bool success = true;
 		for (int j = 0; j < n - 1; ++j)
 		{
-			int temp = a[i][j + 1] - a[i][j];
-			if (temp == 0) ++cnt;
-			else if (temp == 1 && cnt >= l) cnt = 1;// 오르막길
-			else if (temp == -1 && cnt >= 0)cnt = 1 - l;// 내리막길
-			else 
+			int dif = a[i][j + 1] - a[i][j];
+
+			if (dif == 0)
 			{
-				success = false; 
+				++cnt;
+			}
+			else if (dif == 1 && cnt >= l)
+			{
+				cnt = 1;
+			}
+			else if (dif == -1)
+			{
+				for (int k = 0; k < l - 1; ++k)
+				{
+					++j;
+					if (a[i][j + 1] != a[i][j])
+					{
+						success = false;
+						break;
+					}
+				}
+				cnt = 0;
+			}
+			else
+			{
+				success = false;
 				break;
 			}
-
 		}
-		if (!success || cnt < 0)continue;
-		++ret;
+		if (success)
+		{
+			++ret;
+		}
 	}
-
 }
+
 int main()
 {
 	cin >> n >> l;
-
 	for (int i = 0; i < n; ++i)
 	{
 		for (int j = 0; j < n; ++j)
@@ -42,8 +61,9 @@ int main()
 			b[j][i] = a[i][j];
 		}
 	}
-	check(a);
-	check(b);
+
+	go(a);
+	go(b);
 	cout << ret << "\n";
 	return 0;
 }
