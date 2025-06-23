76c7d9bcccd77c24e914d1ce61a0aa2e138ea797
chan9934 <kim75249934@gmail.com>
2025-06-23 09:46:52 +0900

    [update] 1062_가르침 재풀이

--- CodingTest/CodingTest/1062_가르침.cpp
+++ CodingTest/CodingTest/1062_가르침.cpp
@@ -3,53 +3,74 @@
 
 using namespace std;
 
-int n, k, mask;
+int n, k;
+int words[51];
 string s;
-int worlds[51];
 
-int count(int mask)
+void masking(int& mask, char c)
+{
+	mask |= (1 << (c - 'a'));
+}
+int calcul(int mask)
 {
 	int cnt = 0;
-	for (int world : worlds)
+	for (int i = 0; i < n; ++i)
 	{
-		if (world && (world & mask) == world)cnt++;
+		if ((words[i] & mask) == words[i])
+		{
+			++cnt;
+		}
 	}
 	return cnt;
 }
 
 int go(int index, int r, int mask)
 {
-	if (r < 0) return 0;
-	if (index == 26) return count(mask);
-	int ret = 0;
-	int bit = 1 << index;
-	if (!(mask & bit))
+	if (r <= 0 || index == 26)
 	{
-		ret = max( ret, go(index + 1, r - 1, mask | bit));
+		return calcul(mask);
+	}
+	int ret = 0;
+	if (!(mask & (1 << index)))
+	{
+		ret = go(index + 1, r - 1, mask | (1 << (index)));
 	}
 	ret = max(ret, go(index + 1, r, mask));
-
 	return ret;
 }
+
 int main()
 {
 	cin >> n >> k;
 	for (int i = 0; i < n; ++i)
 	{
 		cin >> s;
-		int world = 0;
+		int word = 0;
 		for (char c : s)
 		{
-			world |= (1 <<( c - 'a'));
+			masking(word, c);
 		}
-		worlds[i] = world;
-		//cout << bitset<15>(world) << "\n";
+		/*cout << bitset<26>(word) << "\n";
+		for (int i = 25; i >= 0; --i)
+		{
+			cout << char(i + 'a');
+		}
+		cout << "\n";*/
+		words[i] = word;
 	}
-	mask |= (1 << ('a' - 'a'));
-	mask |= (1 << ('n' - 'a'));
-	mask |= (1 << ('t' - 'a'));
-	mask |= (1 << ('i' - 'a'));
-	mask |= (1 << ('c' - 'a'));
+
+	if (k < 5)
+	{
+		cout << 0 << "\n";
+		return 0;
+	}
+	int mask = 0;
+	masking(mask, 'a');
+	masking(mask, 'n');
+	masking(mask, 't');
+	masking(mask, 'i');
+	masking(mask, 'c');
 	cout << go(0, k - 5, mask) << "\n";
 	return 0;
+
 }
