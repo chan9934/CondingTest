e5545bb8c02538c0d493df970ea2f15b99cca2e8
chan9934 <kim75249934@gmail.com>
2025-06-24 18:20:50 +0900

    [udpate] 1094_막대기 재풀이

--- CodingTest/CodingTest/1094_막대기.cpp
+++ CodingTest/CodingTest/1094_막대기.cpp
@@ -4,33 +4,15 @@ using namespace std;
 
 int x, ret;
 
-void go(int current, int sum, int num)
-{
-	//cout << "current : " << current << " sum : " << sum << " num : " << num << "\n";
-	if (current < 1)
-		return;
-	if (current == x)
-	{
-		ret = 1;
-		return;
-	}
-	if (sum > x)
-		return;
-	if (sum == x)
-	{
-		ret = num;
-		return;
-	}
-
-	go(current / 2, sum + current / 2, num + 1);
-	go(current / 2, sum, num);
-
-}
-
 int main()
 {
 	cin >> x;
-	go(64, 0, 0);
-	cout << ret;
+	while(x != 1)
+	{
+		if (x & 1)
+			++ret;
+		x /= 2;
+	}
+	cout << ret + 1 << "\n";
 	return 0;
 }
