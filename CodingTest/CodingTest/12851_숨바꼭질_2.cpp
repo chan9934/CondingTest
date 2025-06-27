a80adc6de301ca23c84fde924fc9df9e0b5b54fc
chan9934 <kim75249934@gmail.com>
2025-06-27 19:20:24 +0900

    [update] 12851_숨바꼭질_2_재풀이

--- CodingTest/CodingTest/12851_숨바꼭질_2.cpp
+++ CodingTest/CodingTest/12851_숨바꼭질_2.cpp
@@ -1,50 +1,50 @@
-#include <iostream>
-#include <queue>
+	#include <iostream>
+	#include <queue>
 
-using namespace std;
+	using namespace std;
 
-int n, k;
+	int n, k;
 
-int visited[100004];
-int cnt[100004];
+	int visited[100004];
+	int cnt[100004];
 
-void bfs(int x)
-{
-	queue<int>q;
-	q.push(x);
-	visited[x] = 1;
-
-	while (q.size())
+	void bfs(int x)
 	{
-		x = q.front();
-		q.pop();
-		for (int i : { x - 1, x + 1, x * 2})
-		{
-			if (i < 0 || i > 100000)continue;
+		queue<int>q;
+		q.push(x);
+		visited[x] = 1;
 
-			if (visited[i] == 0)
+		while (q.size())
+		{
+			x = q.front();
+			q.pop();
+			for (int i : { x - 1, x + 1, x * 2})
 			{
-				visited[i] = visited[x] + 1;
-				q.push(i);
-				++cnt[i];
-			}
-			else if (visited[i] == visited[x] + 1)
-			{
-				q.push(i);
-				++cnt[i];
+				if (i < 0 || i > 100000)continue;
+
+				if (visited[i] == 0)
+				{
+					visited[i] = visited[x] + 1;
+					q.push(i);
+					++cnt[i];
+				}
+				else if (visited[i] == visited[x] + 1)
+				{
+					q.push(i);
+					++cnt[i];
+				}
 			}
 		}
 	}
-}
-int main()
-{
-	cin >> n >> k;
-	if (n == k)
+	int main()
 	{
-		cout <<0 << "\n" << 1;
+		cin >> n >> k;
+		if (n == k)
+		{
+			cout <<0 << "\n" << 1;
+			return 0;
+		}
+		bfs(n);
+		cout << visited[k] -1 << "\n" << cnt[k];
 		return 0;
 	}
-	bfs(n);
-	cout << visited[k] -1 << "\n" << cnt[k];
-	return 0;
-}
