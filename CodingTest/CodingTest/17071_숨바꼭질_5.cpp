9d86200cfc08739599985fdc76423170fdc54036
chan9934 <kim75249934@gmail.com>
2025-06-27 19:58:01 +0900

    [update] 17071_숨바꼭질_5_재풀이

--- CodingTest/CodingTest/17071_숨바꼭질_5.cpp
+++ CodingTest/CodingTest/17071_숨바꼭질_5.cpp
@@ -1,64 +1,64 @@
-#include <iostream>
-#include <queue>
+	#include <iostream>
+	#include <queue>
 
-using namespace std;
+	using namespace std;
 
-int n, k, t;
+	int n, k, t;
 
-const int max_n = 500000;
+	const int max_n = 500000;
 
-int visited[2][max_n + 4];
+	int visited[2][max_n + 4];
 
-bool bfs(int here)
-{
-	visited[0][here] = 1;
-
-	queue<int>q;
-	q.push(here);
-
-	while (q.size())
+	bool bfs(int here)
 	{
-		int qsize = q.size();
-		++t;
-		k += t;
-		if (k > max_n)
-			return false;
+		visited[0][here] = 1;
 
-		if (visited[t % 2][k])
-		{
-			return true;
-		}
-		for (int i = 0; i < qsize; ++i)
-		{
-			here = q.front();
-			q.pop();
+		queue<int>q;
+		q.push(here);
 
-			for(int next : {here + 1, here -1, here * 2})
+		while (q.size())
+		{
+			int qsize = q.size();
+			++t;
+			k += t;
+			if (k > max_n)
+				return false;
+
+			if (visited[t % 2][k])
 			{
-				if (next < 0 || next > max_n)continue;
-				if (next == k)
-					return true;
-				if (visited[t % 2][next])continue;
-				visited[t % 2][next] = 1;
-				q.push(next);
+				return true;
+			}
+			for (int i = 0; i < qsize; ++i)
+			{
+				here = q.front();
+				q.pop();
+
+				for(int next : {here + 1, here -1, here * 2})
+				{
+					if (next < 0 || next > max_n)continue;
+					if (next == k)
+						return true;
+					if (visited[t % 2][next])continue;
+					visited[t % 2][next] = 1;
+					q.push(next);
+				}
 			}
 		}
+		return false;
 	}
-	return false;
-}
 
-int main()
-{
-	cin >> n >> k;
-
-	if (n == k)
+	int main()
 	{
-		cout << "0";
+		cin >> n >> k;
+
+		if (n == k)
+		{
+			cout << "0";
+			return 0;
+		}
+		if (!bfs(n))
+			cout << -1;
+		else
+			cout << t;
 		return 0;
 	}
-	if (!bfs(n))
-		cout << -1;
-	else
-		cout << t;
-	return 0;
-}
