fdfc6b45f50d58644b16bcee7097cf8cfb1595c9
chan9934 <kim75249934@gmail.com>
2025-06-27 18:59:19 +0900

    [update]16637_괄호_추가하기_재풀이

--- CodingTest/CodingTest/16637_괄호_추가하기.cpp
+++ CodingTest/CodingTest/16637_괄호_추가하기.cpp
@@ -1,60 +1,60 @@
-#include <iostream>
-#include <vector>
-#include <climits>
+	#include <iostream>
+	#include <vector>
+	#include <climits>
 
 
-using namespace std;
+	using namespace std;
 
-int n, ret;
-vector<char>oper;
-vector<int>num;
+	int n, ret;
+	vector<char>oper;
+	vector<int>num;
 
-int calculate(char oper, int l_value, int r_value)
-{
-	if (oper == '+')
-		return l_value + r_value;
-	else if (oper == '-')
-		return l_value - r_value;
-	else
-		return l_value * r_value;
-}
-
-void go(int here, int l_value)
-{
-	if (here == (int)oper.size())
+	int calculate(char oper, int l_value, int r_value)
 	{
-		ret = max(ret, l_value);
-		return;
-	}
-	go(here + 1, calculate(oper[here], l_value, num[here + 1]));
-
-	if(here + 2 <= (int)num.size() - 1)
-	{
-		int temp = calculate(oper[here + 1], num[here + 1], num[here + 2]);
-
-		go(here + 2, calculate(oper[here], l_value, temp));
-	}
-}
-
-int main()
-{
-	ios_base::sync_with_stdio(false);
-	cin.tie(NULL); cout.tie(NULL);
-
-	ret = INT_MIN;
-	cin >> n;
-
-	for (int i = 0; i < n; ++i)
-	{
-		char c;
-		cin >> c;
-		if (i & 1)
-			oper.push_back(c);
+		if (oper == '+')
+			return l_value + r_value;
+		else if (oper == '-')
+			return l_value - r_value;
 		else
-			num.push_back(c - '0');
+			return l_value * r_value;
 	}
 
-	go(0, num[0]);
-	cout << ret << '\n';
-	return 0;
-}
+	void go(int here, int l_value)
+	{
+		if (here == (int)oper.size())
+		{
+			ret = max(ret, l_value);
+			return;
+		}
+		go(here + 1, calculate(oper[here], l_value, num[here + 1]));
+
+		if(here + 2 <= (int)num.size() - 1)
+		{
+			int temp = calculate(oper[here + 1], num[here + 1], num[here + 2]);
+
+			go(here + 2, calculate(oper[here], l_value, temp));
+		}
+	}
+
+	int main()
+	{
+		ios_base::sync_with_stdio(false);
+		cin.tie(NULL); cout.tie(NULL);
+
+		ret = INT_MIN;
+		cin >> n;
+
+		for (int i = 0; i < n; ++i)
+		{
+			char c;
+			cin >> c;
+			if (i & 1)
+				oper.push_back(c);
+			else
+				num.push_back(c - '0');
+		}
+
+		go(0, num[0]);
+		cout << ret << '\n';
+		return 0;
+	}
