d218001e04dfb028c3443210d9d65644b18abc22
chan9934 <kim75249934@gmail.com>
2025-07-04 05:25:11 +0900

    [update] 15926_현욱은_괄호양이야! 스택방식은 이해 못하겠음

--- CodingTest/CodingTest/15926_현욱은_괄호양이야!.cpp
+++ CodingTest/CodingTest/15926_현욱은_괄호양이야!.cpp
@@ -3,8 +3,7 @@
 #include <bitset>
 
 using namespace std;
-int a[200004];
-int n, max_cnt, cnt;
+int n,ret;
 string s;
 stack<int>stk;
 int main()
@@ -12,7 +11,7 @@ int main()
 	ios_base::sync_with_stdio(false);
 	cin.tie(NULL); cout.tie(NULL);
 	cin >> n >> s;
-
+	stk.push(-1);
 	for (int i = 0; i < n; ++i)
 	{
 		if (s[i] == '(')
@@ -21,27 +20,14 @@ int main()
 		}
 		else
 		{
+			stk.pop();
 			if(!stk.empty())
 			{
-				a[stk.top()] = a[i] = 1;
-				stk.pop();
+				ret = max(ret, i - stk.top());
 			}
 		}
 	}
-	for (int i = 0; i < n; ++i)
-	{
-		if (a[i] == 1)
-		{
-			++cnt;
-		}
-		else
-		{
-			max_cnt = max(max_cnt, cnt);
-			cnt = 0;
-		}
-	}
-	max_cnt = max(max_cnt, cnt);
-	cout << max_cnt << "\n";
+	cout << ret << "\n";
 	return 0;
 }
 
