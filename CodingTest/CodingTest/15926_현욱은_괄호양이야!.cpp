fb070f1ba81a9b094ec2e808c733849df959ce34
chan9934 <kim75249934@gmail.com>
2025-07-05 15:39:57 +0900

    [update] 15926_현욱은_괄호양이야! 다시풀어도 못품

--- CodingTest/CodingTest/15926_현욱은_괄호양이야!.cpp
+++ CodingTest/CodingTest/15926_현욱은_괄호양이야!.cpp
@@ -1,33 +1,41 @@
 #include <iostream>
 #include <stack>
-#include <bitset>
 
 using namespace std;
-int n,ret;
+
+int n, ret;
 string s;
-stack<int>stk;
+stack<int> stk;
 int main()
 {
 	ios_base::sync_with_stdio(false);
-	cin.tie(NULL); cout.tie(NULL);
-	cin >> n >> s;
-	stk.push(-1);
+	cin.tie(NULL), cout.tie(NULL);
+	// input string length
+	cin >> n;
+
+	// input string
+	cin >> s;
+	// count max ret
+	int cnt = 0;
 	for (int i = 0; i < n; ++i)
 	{
-		if (s[i] == '(')
+		if (stk.empty() && (s[i] == ')'))
+		{
+			ret = max(ret, cnt);
+			cnt = 0;
+			continue;
+		}
+		if ((s[i] == ')') && !stk.empty())
+		{
+			cnt += 2;
+			stk.pop();
+		}
+		else if(s[i] == '(')
 		{
 			stk.push(i);
 		}
-		else
-		{
-			stk.pop();
-			if(!stk.empty())
-			{
-				ret = max(ret, i - stk.top());
-			}
-		}
 	}
+	ret = max(ret, cnt);
 	cout << ret << "\n";
-	return 0;
+	return  0;
 }
-
