ec9c8112436299de340ee9699258770c1ccc1610
chan9934 <kim75249934@gmail.com>
2025-07-05 12:38:01 +0900

    [update] 3015_오아시스_재결합 못품

---
+++ CodingTest/CodingTest/3015_오아시스_재결합.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+
+using namespace std;
+
+int n, ret;
+
+int a[500004], b[500004];
+
+int main()
+{
+	// Number of People
+	cin >> n;
+
+	// Add heights to a_array
+	for (int i = 0; i < n; ++i)
+	{
+		cin >> a[i];
+	}
+	
+	// Count visible pairs
+	for (int i = 1; i < n; ++i)
+	{
+
+	}
+
+	return 0;
+}
