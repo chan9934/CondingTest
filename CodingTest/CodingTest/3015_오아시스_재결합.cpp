7b77530d2e2a1c8bf9faf2e63b4204a227d5a917
chan9934 <kim75249934@gmail.com>
2025-07-05 15:14:50 +0900

    [update] 3015_오아시스_재결합 (cnt 계산후 pair로 재풀이)

--- CodingTest/CodingTest/3015_오아시스_재결합.cpp
+++ CodingTest/CodingTest/3015_오아시스_재결합.cpp
@@ -7,7 +7,7 @@ using namespace std;
 typedef long long ll;
 ll n, ret, temp;
 
-stack <ll>stk;
+stack <pair<ll,ll>>stk;
 
 int main()
 {
@@ -17,14 +17,25 @@ int main()
 	// Sort
 	for (int i = 0; i < n; ++i)
 	{
+		// add person
 		cin >> temp;
-		while (stk.size() && stk.top() < temp)
+		int cnt = 1;
+		// Stack top is shorter than temp
+		while (stk.size() && stk.top().first <= temp)
 		{
-			ret += 1;
+			ret += stk.top().second;
+			if (stk.top().first == temp)
+			{
+				cnt = stk.top().second + 1;
+			}
+			else
+			{
+				cnt = 1;
+			}
 			stk.pop();
 		}
 		if(stk.size())++ret;
-		stk.push(temp);
+		stk.push({temp, cnt});
 	}
 
 	cout << ret << '\n';
