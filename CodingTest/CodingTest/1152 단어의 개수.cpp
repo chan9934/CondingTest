40e36f6ec990f2a6555380416c919eb379b0a8a4
chan9934 <kim75249934@gmail.com>
2025-07-10 08:19:07 +0900

    [update] 1152 단어의 개수 풀이

---
+++ CodingTest/CodingTest/1152 단어의 개수.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <string>
+#include <algorithm>
+
+using namespace std;
+
+string s;
+char c;
+int ret, temp;
+int main()
+{/*
+	getline(cin, s);
+	for (int i = 0; i < s.size(); ++i)
+	{
+		if (s[i] == ' ')
+			++temp;
+	}
+	cout << s.size() - temp << "\n";*/
+	while (cin >> s)
+	{
+		++ret;
+	}
+	cout << ret;
+	return 0;
+}
