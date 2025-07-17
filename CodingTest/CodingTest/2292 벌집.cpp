2be57f00cdfd0a690185c4e7c5b4fe9580154958
chan9934 <kim75249934@gmail.com>
2025-07-18 08:59:09 +0900

    [update] 2292 벌집 풀이

--- CodingTest/CodingTest/2292 벌집.cpp
+++ CodingTest/CodingTest/2292 벌집.cpp
@@ -2,7 +2,21 @@
 
 using namespace std;
 
+typedef long long ll;
+
+ll n, sum;
+int idx;
 int main()
 {
+	cin >> n;
+	
+	sum = 1;
+	idx = 1;
+	while (n > sum)
+	{
+		sum += (6 * idx);
+		++idx;
+	}
+	cout << idx;
 	return 0;
 }
