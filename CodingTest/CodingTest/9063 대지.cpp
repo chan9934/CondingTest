285befe8b4268a7b2a7a0cfd42afe44ad59fd2ab
chan9934 <kim75249934@gmail.com>
2025-07-19 17:04:40 +0900

    [update] 9063 대지 풀이

---
+++ CodingTest/CodingTest/9063 대지.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+
+#define y1 aaaa
+using namespace std;
+
+int n, x1, y1, min_x, min_y, max_x, max_y;
+
+int main()
+{
+	cin >> n;
+	min_x = min_y = 100000;
+	max_x = max_y = -100000;
+	for (int i = 0; i < n; ++i)
+	{
+		cin >> x1 >> y1;
+		min_x = min(min_x, x1);
+		min_y = min(min_y, y1);
+		max_x = max(max_x, x1);
+		max_y = max(max_y, y1);
+	}
+	cout << (max_x - min_x) * (max_y - min_y) << "\n";
+	return 0;
+}
