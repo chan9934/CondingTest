d7b1c5db1ee1e432da00fe8ad451e6b31fa0afa4
chan9934 <kim75249934@gmail.com>
2025-07-25 21:40:36 +0900

    [update] 10815 숫자 카드 풀이

--- CodingTest/CodingTest/10815 숫자 카드.cpp
+++ CodingTest/CodingTest/10815 숫자 카드.cpp
@@ -2,7 +2,30 @@
 
 using namespace std;
 
+bool a[20000004];
+int n, number, m, check_number;
 int main()
 {
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL), cout.tie(NULL);
+	cin >> n;
+	for (int i = 0; i < n; ++i)
+	{
+		cin >> number;
+		a[number + 10000000] = true;
+	}
+	cin >> m;
+	for (int i = 0; i < m; ++i)
+	{
+		cin >> check_number;
+		if (a[check_number + 10000000] == true)
+		{
+			cout << "1" << " ";
+		}
+		else
+		{
+			cout << "0" << " ";
+		}
+	}
 	return 0;
 }
