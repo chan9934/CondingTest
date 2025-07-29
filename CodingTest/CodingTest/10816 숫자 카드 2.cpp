d724cfc92d1c20e9ecf00a23318d81f566b2fe82
chan9934 <kim75249934@gmail.com>
2025-07-29 15:39:05 +0900

    [update] 1764 듣보잡 풀이

--- CodingTest/CodingTest/10816 숫자 카드 2.cpp
+++ CodingTest/CodingTest/10816 숫자 카드 2.cpp
@@ -5,6 +5,8 @@ int n, m, num;
 int a[20000004];
 int main()
 {
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL), cout.tie(NULL);
 	cin >> n;
 	for (int i = 0; i < n; ++i)
 	{
---
+++ CodingTest/CodingTest/1764 듣보잡.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+#include <vector>
+#include <map>
+#include <algorithm>
+
+using namespace std;
+
+int n, m, ret_num;
+string str;
+map<string, int>list;
+vector<string> ret;
+int main()
+{
+	cin >> n >> m;
+	for (int i = 0; i < n; ++i)
+	{
+		cin >> str;
+		++list[str];
+	}
+	for (int i = 0; i < m; ++i)
+	{
+		cin >> str;
+		++list[str];
+		if (list[str] == 2)
+			++ret_num;
+	}
+	for (const pair<string, int>& element : list)
+	{
+		if (element.second == 2)
+			ret.push_back(element.first);
+	}
+	cout << ret_num << "\n";
+	sort(ret.begin(), ret.end());
+	for (const string& element : ret)
+	{
+		cout << element << "\n";
+	}
+	return 0;
+}
